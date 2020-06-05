a34991826ec1ddefc4d83eca4bd8a79c20effc0c
Solongo <soogii1005@gmail.com>
2020-06-04 22:49:42 -0700

    created app-grocery component, added bootstrap, add items functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroceryComponent } from './app.grocery';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [GroceryComponent, AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GroceryComponent, AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
