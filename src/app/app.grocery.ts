fb2e6eefce92d6d0dbaf6235096654410fa8acb4
Solongo <soogii1005@gmail.com>
2020-06-12 22:47:26 -0700

    added delete, edit, strike functions

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -3,18 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-grocery',
   templateUrl: './app.grocery.html',
-  styleUrls: [
-    '../assets/bootstrap/css/bootstrap.min.css'
-  ]
+  styleUrls: ['../assets/bootstrap/css/bootstrap.min.css']
 })
-
 export class GroceryComponent {
-  task: string;
+  task = {
+    name: '',
+    id: 0
+  };
   tasks = [];
 
   onClick() {
-    this.tasks.push({name: this.task});
-    this.task = '';
+    if (this.task.id == 0) {
+      this.tasks.push({name: this.task.name, id: new Date().getTime() });
+    }
+    this.task = {
+      name: '',
+      id: 0
+    }
+  }
+
+  onStrike(item) {
+    for(let i=0; i < this.tasks.length; i++) {
+      if(item.id == this.tasks[i].id) {
+        if(this.tasks[i].strike) {
+          this.tasks[i].strike = false;
+        } else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
+
+  onEdit(item) {
+    this.task.name = item;
+    console.log(this.task);
+  }
+
+  onDelete(item) {
+    for (let i=0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        this.tasks.splice(i, 1);
+        break;
+      }
+    }
   }
 }
-
